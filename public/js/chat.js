945b117650d697bbb8e5eb51a8eaf874349b557a
Arafat Tai <tai.arafat.at@gmail.com>
2020-03-23 21:39:10 +0530

    initiated a socket io connection

---
+++ public/js/chat.js
@@ -0,0 +1,2 @@
+// Connects to socket on server
+io()
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-const app = require('express')
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
+
+// Path for the public directory
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// Port number for our app
+const port = process.env.PORT || 3000
+
+// Getting instance of express server
+const app = express()
+
+// Setting up http express server for socket programming
+const server = http.createServer(app)
+
+// Getting instance of socket.io
+const io = socketio(server)
+
+// Listening on 'connection' event on socket
+io.on('connection', () => {
+    console.log('New WebSocket connection');
+})
+
+// Setting up static public directory to serve
+app.use(express.static(publicDirectoryPath))
+
+server.listen(port, () => {
+    console.log('Server is up on port', port);
+})
